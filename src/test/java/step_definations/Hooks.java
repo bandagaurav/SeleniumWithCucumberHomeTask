4985e95a66213a8f2a95877692cfe6353982f042
Gaurav Mahajan <gaurav_mahajan@epam.com>
2024-09-03 16:07:24 +0530

    first commit

--- src/test/java/step_definations/Hooks.java
+++ src/test/java/step_definations/Hooks.java
@@ -1,8 +1,8 @@
 package step_definations;
 
 import cucumber.api.Scenario;
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
