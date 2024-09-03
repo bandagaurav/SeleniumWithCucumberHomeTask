d6416f084ef7f2aeef03389b47c6129d8d856b0b
Gaurav Mahajan <gaurav_mahajan@epam.com>
2024-09-03 14:53:44 +0530

    first commit

--- src/test/java/step_definations/Run_Tests.java
+++ src/test/java/step_definations/Run_Tests.java
@@ -1,6 +1,5 @@
 package step_definations;
 
-import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.FeatureWrapper;
 import io.cucumber.testng.PickleWrapper;
@@ -16,7 +15,7 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 
-/*@ExtendedCucumberOptions(
+@ExtendedCucumberOptions(
         jsonReport = "target/81/cucumber.json",
         jsonUsageReport = "target/81/cucumber-usage.json",
         usageReport = true,
@@ -27,8 +26,8 @@ import org.testng.annotations.Test;
         pdfPageSize = "A4 Landscape",
         toPDF = false,
         outputFolder = "target/81",
-        retryCount = 0)*/
-@RunWith(Cucumber.class)
+        retryCount = 0)
+@RunWith(ExtendedCucumber.class)
 @CucumberOptions(features = { "src/test/resources/feature" },
         tags = {"@login"},plugin = {
         		"usage:target/81/cucumber-usage.json","html:target/81", "json:target/cucumber-reports/CucumberTestReport.json","json:target/81/cucumber.json",
@@ -41,9 +40,9 @@ public class Run_Tests {
 	public static int retries = 0;
     public Run_Tests() {
     }
-    /*@RetryAcceptance
+    @RetryAcceptance
     public static boolean retryCheck(Throwable e) {
         return true;
-    }*/
+    }
 }
 
