fec6fe6828056019eb76dadebe83600713e8081d
Gaurav Mahajan <gaurav_mahajan@epam.com>
2024-09-03 13:56:25 +0530

    first commit

--- src/Prep/Lesson4/HomeTask9/AgeCalculator.java
+++
@@ -1,9 +0,0 @@
-package Prep.Lesson4.HomeTask9;
-
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.function.Function;
-
-public class AgeCalculator {
-    public static Function<LocalDate, Integer> calculateAge = birthDate -> Period.between(birthDate, LocalDate.now()).getYears();
-}
--- src/Prep/Lesson4/HomeTask9/DateChecker.java
+++
@@ -1,8 +0,0 @@
-package Prep.Lesson4.HomeTask9;
-
-import java.time.LocalDate;
-import java.util.function.Predicate;
-
-public class DateChecker {
-    public static Predicate<LocalDate> isYesterday = date -> date.equals(LocalDate.now().minusDays(1));
-}
